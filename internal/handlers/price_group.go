9b23b86b541b7707f05dc774a09d3729cbd3e7ce
Snezhana Dorogova <snezhana.dorogova@gmail.com>
2020-01-21 12:58:23 +0300

    docs: update openapi.yaml

--- internal/handlers/price_group.go
+++ internal/handlers/price_group.go
@@ -4,6 +4,7 @@ import (
 	"github.com/ProtocolONE/go-core/v2/pkg/logger"
 	"github.com/ProtocolONE/go-core/v2/pkg/provider"
 	"github.com/labstack/echo/v4"
+	"github.com/paysuper/paysuper-billing-server/pkg/proto/billing"
 	"github.com/paysuper/paysuper-billing-server/pkg/proto/grpc"
 	"github.com/paysuper/paysuper-management-api/internal/dispatcher/common"
 	"net/http"
